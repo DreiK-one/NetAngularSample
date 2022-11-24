237526157798790ae7eed69da0884337477ba17e
DreiK-one <Dreikone1@mail.ru>
2022-11-24 13:56:16 +0100

    Add basic projects

---
+++ InspectionAPIApp/InspectionAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using InspectionAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
