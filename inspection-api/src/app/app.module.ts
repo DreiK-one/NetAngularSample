bd398bec23dcb7678f03432c9062a517d4f98dd6
DreiK-one <Dreikone1@mail.ru>
2022-11-25 19:09:05 +0100

    Refactoring

--- inspection-api/src/app/app.module.ts
+++ inspection-api/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { InspectionComponent } from './inspection/inspection.component';
 import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
 import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
-import { InspectionApiService } from './services/api.service';
+import { ApiService } from './services/api.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { InspectionApiService } from './services/api.service';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [InspectionApiService],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
