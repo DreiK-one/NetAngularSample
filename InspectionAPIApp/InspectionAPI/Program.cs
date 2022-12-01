e9a0a9c251d59e226cc70e95fb46f5ca2635fbb3
DreiK-one <Dreikone1@mail.ru>
2022-12-01 10:38:00 +0100

    Register services/repositories

--- InspectionAPIApp/InspectionAPI/Program.cs
+++ InspectionAPIApp/InspectionAPI/Program.cs
@@ -1,5 +1,8 @@
 using InspectionAPI.Core.Interfaces;
+using InspectionAPI.Core.Interfaces.Data;
 using InspectionAPI.Data;
+using InspectionAPI.Data.Entities;
+using InspectionAPI.DataAccess;
 using InspectionAPI.Domain.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +12,11 @@ var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IBaseRepository<Status>, StatusRepository>();
+builder.Services.AddScoped<IBaseRepository<Inspection>, InspectionRepository>();
+builder.Services.AddScoped<IBaseRepository<InspectionType>, InspectionTypeRepository>();
+
 builder.Services.AddScoped<IInspectionService, InspectionService>();
 builder.Services.AddScoped<IInspectionTypeService, InspectionTypeService>();
 builder.Services.AddScoped<IStatusService, StatusService>();
